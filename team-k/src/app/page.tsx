21bae6b36110985afe0c4c30e8e84ff59c7af5dd
Bhargavi Chinni <bchinni@unc.edu>
2025-03-17 16:22:11 -0400

    hover over language change

--- team-k/src/app/page.tsx
+++ team-k/src/app/page.tsx
@@ -1,11 +1,17 @@
+"use client";
+import "@/utils/i18n";
+import i18next from "i18next";
+import { useTranslation } from "react-i18next";
 import Image from "next/image";
 
 export default function Home() {
+  const {t} = useTranslation();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       
       {/* Centered Heading */}
-      <h1 className="text-7xl font-bold text-center w-full">Welcome to NeuroChat!</h1>
+      <h1 className="text-7xl font-bold text-center w-full">{t('welcomeMessage')}</h1>
 
       <main className="grid grid-cols-2 gap-8 row-start-2 items-center">
         {/* Left Column */}
@@ -24,22 +30,28 @@ export default function Home() {
           <h3 style={{ fontSize: "24px", fontWeight: "bold" }}>Pick a language to begin:</h3>
 
           <div className="flex flex-row gap-4">
-            <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              English
-            </a>
-            <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Español
-            </a>
+            <div onMouseOverCapture={() => i18next.changeLanguage('en')}>
+              <a
+                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+                //href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                English
+              </a>
+            </div>
+            
+            <div onMouseOverCapture={() => i18next.changeLanguage('es')}>
+              <a
+                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+                //href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Español
+              </a>
+            </div>
+            
           </div>
         </div>
       </main>
