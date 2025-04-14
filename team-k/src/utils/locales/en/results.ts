7aff69e6a965a6b7ab689d0d29988c2b3d93e775
Bhargavi Chinni <bchinni@unc.edu>
2025-04-14 15:22:45 -0400

    reformatted results ts files

--- team-k/src/utils/locales/en/results.ts
+++ team-k/src/utils/locales/en/results.ts
@@ -1,36 +1,49 @@
 const results = {
-    // disease names
-    parkinsonsDisease: "Parkinson's Disease",
-    als: "Amyotrophic Lateral Sclerosis (ALS)",
-    alzheimersDisease: "Alzheimer's Disease",
-    multipleSclerosis: "Multiple Sclerosis",
-    huntingtonsDisease: "Huntington's Disease",
-    ataxia: "Ataxia",
-    motorNeuronDisease: "Motor Neuron Disease (MND)",
-    multipleSystemAtrophy: "Multiple System Atrophy (MSA)",
-    progressiveSupranuclearPalsy: "Progressive Supranuclear Palsy (PSP)",
-
-    // disease treatments
-    parkinsonsDiseaseTreatment: "Parkinson's disease can't be cured, but medicines can help control the symptoms. Medicines often work very well. When medicine is no longer helping, some people may have surgery. Your healthcare team also may recommend aerobic exercise, physical therapy that focuses on balancing and stretching, and speech therapy.",
-    alsTreatment: "Treatments can't reverse the damage of ALS, but they can slow the progression of symptoms. They also can help prevent complications and make you more comfortable and independent. You might need a team of health care providers and doctors trained in many areas to provide your care. The team works together to prolong your survival and improve your quality of life. Your team works to select the right treatments for you. You have the right to choose or refuse any of the treatments suggested.",
-    alzheimersDiseaseTreatment: "Treatments for Alzheimer's disease include medicines that can help with symptoms and newer medicines that can help slow decline in thinking and functioning. These newer medicines are approved for people with early Alzheimer's disease",
-    multipleSclerosisTreatment: "There is no cure for multiple sclerosis. Treatment typically focuses on speeding recovery from attacks, reducing relapses, slowing the progression of the disease and managing MS symptoms. Some people have such mild symptoms that no treatment is necessary.",
-    huntingtonsDiseaseTreatment: "No treatments can alter the course of Huntington's disease. But medicines can lessen some symptoms of movement and mental health conditions. And multiple interventions can help a person adapt to changes in abilities for a certain amount of time.",
-    ataxiaTreatment: "Ataxia treatment depends on the cause. If ataxia is caused by a condition such as vitamin deficiency or celiac disease, treating the condition may help improve symptoms. If ataxia results from chickenpox or other viral infections, it is likely to resolve on its own.",
-    motorNeuronDiseaseTreatment: "There's no cure for motor neuron disease, but treatment can help reduce the impact the symptoms have on your life. Care typically involves a team of specialists, including a general practitioner",
-    multipleSystemAtrophyTreatment: "Treatment for multiple system atrophy (MSA) involves managing your symptoms. There's no cure for MSA. Managing the disease can make you as comfortable as possible and help you maintain your body functions.",
-    progressiveSupranuclearPalsyTreatment: "Although there is no cure for progressive supranuclear palsy, treatments are available to help ease symptoms of the disorder.",
-
-    // disease symptoms
-    parkinsonsDiseaseSymptoms: "Tremor; Slowed movement; Rigid muscles; Poor posture; Loss of automatic movement; Slurred speech; Writing changes; Depression; Poor balance; Loss of bladder or bowel control; Sleep issues",
-    alsSymptoms: "Trouble walking; Leg/ankle/foot weakness; Hand fine-motor weakness or clumsiness; Slurred speech; Trouble swallowing; Muscle cramps and twitching (arms, shoulders, tongue); Emotional instability (inappropriate crying/laughing); Thinking or behavioral changes",
-    alzheimersDiseaseSymptoms: "Memory loss; Trouble concentrating and thinking; Poor decision-making and judgment; Difficult planning and performing familiar tasks; Depression; Social withdrawal; Mood swings; Sleep issues",
-    multipleSclerosisSymptoms: "Numbness; Muscle weakness; Trouble walking; Blurred, double, or loss of vision",
-    huntingtonsDiseaseSymptoms: "Involuntary movements (chorea); Trouble focusing eyes; Trouble walking; Poor balance; Trouble swallowing; Lack of impulse control; Trouble organizing/prioritizing/focusing; Depression; Obsessive-compulsive disorder; Mania; Poor posture; Slurred speech; Uncontrolled eye movements",
-    ataxiaSymptoms: "Poor coordination; Poor balance; Trouble walking; Hand fine-motor weakness or clumsiness; Slurred speech; Uncontrolled eye movements; Trouble swallowing",
-    motorNeuronDiseaseSymptoms: "Leg/ankle/foot weakness; Slurred speech; Difficulty swallowing; Muscle cramps and twitches; Weight loss; Emotional instability (inappropriate crying/laughing)",
-    multipleSystemAtrophySymptoms: "Tremors; Trouble walking; Rigid muscles; Poor balance; Loss of automatic movement; Slurred speech; Blurred, double, or loss of vision; Trouble swallowing; Low blood pressure; Loss of bladder or bowel control; Reduced sweating; Sleep issues; Loss of interest in sex; Emotional instability (inappropriate crying/laughing); Poor posture",
-    progressiveSupranuclearPalsySymptoms: "Poor balance; Trouble focusing eyes; Rigid muscles; Slurred speech; Trouble swallowing; Sensitivity to bright light; Sleep issues; Emotional instability (inappropriate crying/laughing); Trouble concentrating and thinking; Depression; Characteristic surprised/frightened facial expression; Dizziness",
-};
-
-export default results;
+    parkinsonsDisease: {
+      name: "Parkinson's Disease",
+      symptoms: "Tremor; Slowed movement; Rigid muscles; Poor posture; Loss of automatic movement; Slurred speech; Writing changes; Depression; Poor balance; Loss of bladder or bowel control; Sleep issues",
+      treatment: "Parkinson's disease can't be cured, but medicines can help control the symptoms. Medicines often work very well. When medicine is no longer helping, some people may have surgery. Your healthcare team also may recommend aerobic exercise, physical therapy that focuses on balancing and stretching, and speech therapy.",
+    },
+    als: {
+      name: "Amyotrophic Lateral Sclerosis (ALS)",
+      symptoms: "Trouble walking; Leg/ankle/foot weakness; Hand fine-motor weakness or clumsiness; Slurred speech; Trouble swallowing; Muscle cramps and twitching (arms, shoulders, tongue); Emotional instability (inappropriate crying/laughing); Thinking or behavioral changes",
+      treatment: "Treatments can't reverse the damage of ALS, but they can slow the progression of symptoms. They also can help prevent complications and make you more comfortable and independent. You might need a team of health care providers and doctors trained in many areas to provide your care. The team works together to prolong your survival and improve your quality of life. Your team works to select the right treatments for you. You have the right to choose or refuse any of the treatments suggested.",
+    },
+    alzheimersDisease: {
+      name: "Alzheimer's Disease",
+      symptoms: "Memory loss; Trouble concentrating and thinking; Poor decision-making and judgment; Difficult planning and performing familiar tasks; Depression; Social withdrawal; Mood swings; Sleep issues",
+      treatment: "Treatments for Alzheimer's disease include medicines that can help with symptoms and newer medicines that can help slow decline in thinking and functioning. These newer medicines are approved for people with early Alzheimer's disease",
+    },
+    multipleSclerosis: {
+      name: "Multiple Sclerosis",
+      symptoms: "Numbness; Muscle weakness; Trouble walking; Blurred, double, or loss of vision",
+      treatment: "There is no cure for multiple sclerosis. Treatment typically focuses on speeding recovery from attacks, reducing relapses, slowing the progression of the disease and managing MS symptoms. Some people have such mild symptoms that no treatment is necessary.",
+    },
+    huntingtonsDisease: {
+      name: "Huntington's Disease",
+      symptoms: "Involuntary movements (chorea); Trouble focusing eyes; Trouble walking; Poor balance; Trouble swallowing; Lack of impulse control; Trouble organizing/prioritizing/focusing; Depression; Obsessive-compulsive disorder; Mania; Poor posture; Slurred speech; Uncontrolled eye movements",
+      treatment: "No treatments can alter the course of Huntington's disease. But medicines can lessen some symptoms of movement and mental health conditions. And multiple interventions can help a person adapt to changes in abilities for a certain amount of time.",
+    },
+    ataxia: {
+      name: "Ataxia",
+      symptoms: "Poor coordination; Poor balance; Trouble walking; Hand fine-motor weakness or clumsiness; Slurred speech; Uncontrolled eye movements; Trouble swallowing",
+      treatment: "Ataxia treatment depends on the cause. If ataxia is caused by a condition such as vitamin deficiency or celiac disease, treating the condition may help improve symptoms. If ataxia results from chickenpox or other viral infections, it is likely to resolve on its own.",
+    },
+    motorNeuronDisease: {
+      name: "Motor Neuron Disease (MND)",
+      symptoms: "Leg/ankle/foot weakness; Slurred speech; Difficulty swallowing; Muscle cramps and twitches; Weight loss; Emotional instability (inappropriate crying/laughing)",
+      treatment: "There's no cure for motor neuron disease, but treatment can help reduce the impact the symptoms have on your life. Care typically involves a team of specialists, including a general practitioner",
+    },
+    multipleSystemAtrophy: {
+      name: "Multiple System Atrophy (MSA)",
+      symptoms: "Tremors; Trouble walking; Rigid muscles; Poor balance; Loss of automatic movement; Slurred speech; Blurred, double, or loss of vision; Trouble swallowing; Low blood pressure; Loss of bladder or bowel control; Reduced sweating; Sleep issues; Loss of interest in sex; Emotional instability (inappropriate crying/laughing); Poor posture",
+      treatment: "Treatment for multiple system atrophy (MSA) involves managing your symptoms. There's no cure for MSA. Managing the disease can make you as comfortable as possible and help you maintain your body functions.",
+    },
+    progressiveSupranuclearPalsy: {
+      name: "Progressive Supranuclear Palsy (PSP)",
+      symptoms: "Poor balance; Trouble focusing eyes; Rigid muscles; Slurred speech; Trouble swallowing; Sensitivity to bright light; Sleep issues; Emotional instability (inappropriate crying/laughing); Trouble concentrating and thinking; Depression; Characteristic surprised/frightened facial expression; Dizziness",
+      treatment: "Although there is no cure for progressive supranuclear palsy, treatments are available to help ease symptoms of the disorder.",
+    },
+  };
+  
+export default results;  
